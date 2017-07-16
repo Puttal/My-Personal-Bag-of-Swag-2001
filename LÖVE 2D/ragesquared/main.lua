e3a18c0c0d24a4ff7d19858873c85b15fe827c8a
Puttal <mlassheikki@gmail.com>
2017-07-16 13:47:37 +0300

    Made the game even harder
    also added some cool pulse effects to circles

--- LÖVE 2D/ragesquared/main.lua
+++ LÖVE 2D/ragesquared/main.lua
@@ -1,15 +1,15 @@
 local timeleft = 3
+local fps = 0
+local pulsefactor = 15 --sets how big the sqaure's pulse is
+local pulsestate = 0 --how far the pulse is (kind of)
 local timemax = timeleft
 local wx, wy = love.window.getMode() --wx and wy are the window x and y
 local gamestarted = false
-bgmusic = love.audio.newSource( 'thingy.wav', 'stream' )
-gameover = love.audio.newSource("sfx/scratch.wav","static")
-click = love.audio.newSource("sfx/click.wav","static")
-local coloredtext = {
-  color1 = {255,0,0},
-  string1 = "window"
-}
+local bgmusic = love.audio.newSource( 'thingy.wav', 'stream' )
+local gameover = love.audio.newSource("sfx/scratch.wav","static")
+local click = love.audio.newSource("sfx/click.wav","static")
 local timeplayed = 0
+local sqoci = false --Chooses if the bad shape is square or circle
 local sq = { --The square
     x = wx/2 - 50,
     y = wy/2 - 50,
@@ -17,43 +17,90 @@ local sq = { --The square
     h = 100, --Height
     color = {255,255,255}, --Color table, red, green and blue. Can also have alpha value
 }
+local bsq = { --The bad square
+active = false, --For easier drawing and checking for clicks
+x = wx/2 - 50,
+y = wy/2 - 50,
+w = 100, --Width
+h = 100, --Height
+color = {255,0,0}, --Color table, red, green and blue. Can also have alpha value
+}
+local bci = { --The bad circle
+active = false,
+x = wx/2 - 50,
+y = wy/2 - 50,
+r = 50, --Radius
+color = {255,255,255},
+}
+love.graphics.setNewFont(36)
 
 function love.draw()
-    love.graphics.print(string.format("%.2f", timeplayed), wx/2-50, wy/2-25, 0, 3, 3)
+  love.graphics.setColor(255,0,0)
+    love.graphics.print(string.format("%.2f", timeplayed), wx/2-50, wy/2-25, 0)
     love.graphics.setColor(sq.color) --Sets the current color
-    love.graphics.rectangle("fill", sq.x, sq.y, sq.w, sq.h) --Draws the square. Fill means
-    --that the whole square will be filled. "line" would draw an outline
+    love.graphics.rectangle("fill", sq.x - pulsestate/2, sq.y - pulsestate/2, sq.w + pulsestate, sq.h + pulsestate) --Draws the square. Fill means
+    --that the whole square will be filled. "line" would draw an outline. Pulsestate added to make the pulse
+    if bsq.active then
+      love.graphics.setColor(bsq.color)
+      love.graphics.rectangle("fill", bsq.x - pulsestate/2, bsq.y - pulsestate/2, bsq.w + pulsestate, bsq.h + pulsestate)
+    end
+    if bci.active then
+      love.graphics.setColor(bci.color)
+      love.graphics.circle("fill", bci.x, bci.y, bci.r + pulsestate/2)
+    end
     love.graphics.setColor(255,255,255)
     love.graphics.setBackgroundColor(150-timeleft*150/timemax,0,0)
-    love.graphics.print(coloredtext)
-
 end
 
 function love.mousepressed(x, y, button)
     if button == 1 then --Button 1 is left mouse.
-        if x > sq.x and
-             x < sq.x + sq.w and
-             y > sq.y and
-             y < sq.y + sq.h then
-             sq.x = math.random(0, wx - sq.w) --Randomises the x and y of the box, but makes sure
-             sq.y = math.random(0, wy - sq.h) --that the square will always be on the window
-             sq.color = {math.random(0, 255),math.random(0, 255),math.random(0, 255)} --Sets a
+        if  x > sq.x and
+            x < sq.x + sq.w and
+            y > sq.y and
+            y < sq.y + sq.h then
+             sq.x = math.random(pulsefactor, wx - sq.w - pulsefactor) --Randomises the x and y of the box, but makes sure
+             sq.y = math.random(pulsefactor, wy - sq.h - pulsefactor) --that the square will always be on the window
+             --sq.color = {math.random(0, 255),math.random(0, 255),math.random(0, 255)} --Sets a
              --random color for the next box
              timeleft    = timeleft + 1
              gamestarted = true
              bgmusic:play()
+             click:stop()
              click:play()
+             if timeplayed > 15 then --bad shapes start appearing
+               sqoci = 0 == math.ceil(math.random(0, 100)) % 2
+               bsq.active = not sqoci
+               bci.active = sqoci
+               bsq.x = math.random(pulsefactor, wx - bsq.w - pulsefactor)
+               bsq.y = math.random(pulsefactor, wy - bsq.h - pulsefactor)
+               bci.x = math.random(pulsefactor + bci.r, wx - bci.r - pulsefactor)
+               bci.y = math.random(pulsefactor + bci.r, wy - bci.r - pulsefactor)
+             end
+          else if bsq.active and
+             x > bsq.x and
+             x < bsq.x + bsq.w and
+             y > bsq.y and
+             y < bsq.y + bsq.h then
+              timeleft = 0
+          else if bci.active and
+            bci.r > math.sqrt(math.pow(x - bci.x, 2) + math.pow(y - bci.y, 2)) then
+              timeleft = 0
+            end
+          end
         end
     end
 end
 
 function love.update(Dt)
   if gamestarted then
-    timeleft = timeleft    - Dt * (30+timeplayed)/30
+    timeleft = timeleft - Dt * (30+timeplayed)/30
     timeplayed = timeplayed + Dt
+    pulsestate = pulsefactor * (math.abs(math.sin(4*math.pi*timeplayed))+math.abs(math.sin(2*math.pi*(timeplayed+0.125)))+math.abs(math.sin(math.pi*(timeplayed+0.375))))/3
     if timeleft > timemax then timeleft = timemax end
   end
-  if timeleft < 0 then
+  if timeleft <= 0 then
+    bsq.active = false
+    bci.active = false
     love.audio.stop( )
     gameover:play()
     gamestarted = false
